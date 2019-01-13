abc92de650df5d346e8af4bb5781a0d0380b9581
perezllas <cristianperezllasera@gmail.com>
2019-01-13 21:07:45 +0100

    Add files via upload

--- code.R
+++ code.R
@@ -17,33 +17,37 @@ library(stringr)
 library(ggplot2)
 
 
+link <- GET("https://www.mediawiki.org/wiki/MediaWiki")
 
 ### 1.2 Analisis de el contenido de la web
 
 ### 1.3.	Extracción de enlaces
-
-link <- GET("https://www.mediawiki.org/wiki/MediaWiki")
-page <- content(link, "text", encoding = "ISO-8859-1")
-parse <- read_html(page)
-links <- xml_find_all(parse, "//@href") 
-links <- xml_text(links)
-df <- data.frame(url=character(), status=integer(), stringsAsFactors = FALSE)
-de <- list()
+extraer_links <- function(link){
+  
+  page <- content(link, "text", encoding = "ISO-8859-1")
+  parse <- read_html(page)
+  links <- xml_find_all(parse, "//@href") 
+  links <- xml_text(links)
+  df <- data.frame(url=character(), status=integer(), stringsAsFactors = FALSE)
+  de <- list()
+}
 
 ### 1.4 Exploración de enlaces
 
-for(item in links){
-  de <- list()
-  https_t <- str_detect(item, "htt")
-  if(https_t == TRUE){
-    status = HEAD(item)
-    status = status_code(status)
-    print(status)
-    #item <- as.factor(item)
-    print(item)
-    de <- c(de, item)
-    de <- c(de, status=status)
-    df[nrow(df) + 1,] = de
+exploracion_datos <- function(links){
+  for(item in links){
+    de <- list()
+    https_t <- str_detect(item, "http")
+    if(https_t == TRUE){
+      status = HEAD(item)
+      status = status_code(status)
+      print(status)
+      #item <- as.factor(item)
+      print(item)
+      de <- c(de, item)
+      de <- c(de, status=status)
+      df[nrow(df) + 1,] = de
+    }
   }
 }
 
@@ -61,4 +65,4 @@ bar_plot(df)
 
 ### 2.3 Pie Chart
 
-pie_chart(df$status)
+pie_chart(df$url)
